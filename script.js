1f44b0f4f9712b2f3aca922597af311519418249
BOUZID Ahmed <ahmahm@hotmail.co.jp>
2024-11-01 12:21:53 +0100

    fin

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-fetch("year-back-app.vercel.app")
+fetch("https://year-back-app.vercel.app/years")
   .then((response) => response.json())
   .then((data) => {
     document.querySelector(".year").textContent = `${data.Year}`;
   });
+
+document.querySelector("#year").textContent = `${data.Year}`;
